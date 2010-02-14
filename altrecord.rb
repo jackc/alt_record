df30d8dde2a42462e473713a90a3f79154930d05
unknown <Jack@.(none)>
2010-02-14 15:39:00 -0600

    Added save.

--- altrecord.rb
+++ altrecord.rb
@@ -15,48 +15,93 @@ module AltRecord
   
   end
   
-  class Table
-    def initialize( database, table_name )
-      @database = database
-      @table_name = table_name
-      @columns = []
+  module Base
+    def self.included(other)
+      other.extend( ClassMethods )
+      other.instance_eval do
+        @database = AltRecord::Database.new :host => 'localhost', :dbname => 'Jack', :user => 'Jack', :password => 'Jack'
+        @table_name = ""
+        @columns = []
+      end
     end
-    
-    def map_column( column_name )
-      c = Column.new
-      c.name = column_name
-      @columns << c
-      
-    end
-    
-    def find( id )
-      @database.exec( "SELECT #{@columns.map { |c| c.name }.join( ", " )} FROM #{@table_name} WHERE id=$1", [ id.to_s ] )
-    end
-    
-    def all
-      ds = DataSet.new
-      rows = @database.exec( "SELECT #{@columns.map { |c| c.name }.join( ", " )} FROM #{@table_name}" )
-      rows.each do |r|        
-        ds.records.push( ds, r )
+  
+    module ClassMethods
+      def set_table_name( _table_name )
+        @table_name = _table_name
       end
       
-      ds
+      def table_name
+        @table_name
+      end
+      
+      def database
+        @database
+      end
+      
+      def map_column( column_name )
+        c = Column.new
+        c.name = column_name
+        @columns << c
+        
+        class_eval <<-END_EVAL
+          def #{column_name}
+            @ar_#{column_name}
+          end
+          
+          def #{column_name}=(v)
+            @ar_#{column_name} = v
+          end
+        END_EVAL
+      end
+      
+      def columns
+        @columns
+      end
+            
+      def find( id )
+        pg_result = @database.exec( "SELECT #{@columns.map { |c| c.name }.join( ", " )} FROM #{@table_name} WHERE id=$1", [ id.to_s ] )
+        r = new
+        @columns.each do |c|
+          r.send("#{c.name}=", pg_result[0][c.name])
+        end
+        r
+      end
+      
+      def all
+        ds = DataSet.new
+        rows = @database.exec( "SELECT #{@columns.map { |c| c.name }.join( ", " )} FROM #{@table_name}" )
+        rows.each do |r|        
+          ds.records.push( ds, r )
+        end
+        
+        ds
+      end
     end
     
-  end
+    def initialize
+      @new_record = true
+    end
+    
+    def new_record?
+      @new_record
+    end
+    
+    def save
+      if new_record?
+        sql = "INSERT INTO #{self.class.table_name} ("
+        sql << self.class.columns.map { |c| c.name }.join(", ")
+        sql << ") VALUES ("
+        sql << (1..self.class.columns.size).map { |n| "$#{n}" }.join(", ")
+        sql << ")"
+        self.class.database.exec sql, self.class.columns.map { |c| send(c.name) }
+      else
+      
+      end
+    end
+	end
   
   class Column
     attr_accessor :name
-  end
-  
-  class Record
-    attr_reader :data_set
-    attr_reader :attributes
-    
-    def initialize( _data_set, _attributes )
-      @data_set = _data_set
-      @attributes = attributes.clone    
-    end
   end
   
   class DataSet
@@ -68,24 +113,18 @@ module AltRecord
   end
 end
 
-DB = AltRecord::Database.new :host => 'localhost', :dbname => 'Jack', :user => 'Jack', :password => 'Jack'
-
-Person = AltRecord::Table.new DB, "people"
-Person.map_column "id"
-Person.map_column "last_name"
-Person.map_column "first_name"
-
-#results = DB.exec "select *, 1.2346549846456165465::float8 from people"
-
-results = Person.find 1
-
-p results.fsize(0)
-p results.fsize(1)
-p results.fsize(2)
-
-results.each do |r|
-  p r
-  p r["float8"].to_f
+class Person
+  include AltRecord::Base
+  
+  set_table_name "people"
+  
+  map_column "id"
+  map_column "last_name"
+  map_column "first_name"
 end
 
-p Person.all
+p = Person.new
+p.id = 2
+p.last_name = "Smith"
+p.first_name = "John"
+p.save
